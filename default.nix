c94ac48b2622379e2617d5f66ef4ce765925b557
Sergey Mironov <grrwlf@gmail.com>
2021-03-25 22:48:06 +0300

    Add utilitary files

---
+++ default.nix
@@ -0,0 +1,36 @@
+{ pkgs ?  import <nixpkgs> {}
+, python ? pkgs.python37
+, doCheck ? true
+} :
+
+with python.pkgs;
+buildPythonPackage {
+  pname = "qsim";
+  version = "1.0.0";
+  src = builtins.filterSource (
+    path: type: !( baseNameOf path == "build" && type == "directory" ) &&
+                !( baseNameOf path == "result" )
+    ) ./.;
+
+  preConfigure = ''
+    if ! test -d /build/qsim/.git ; then
+      echo "Looks like QSim is a submodule of some other repo."\
+           "\`nix-build\` is unable to detect its version, unfortunately."\
+           "Please checkout QSim as a normal Git repository"\
+           "and retry." >&2
+      exit 1
+    fi
+  '';
+
+  buildInputs = [ setuptools_scm ];
+
+  checkInputs = [ pytest pytest-mypy hypothesis ];
+
+  checkPhase = ''
+    pytest
+  '';
+
+  inherit doCheck;
+}
+
+
